85dc13ea49dab4aa40a90c5521b014f92af09da8
d-cuzya <Admin@DESKTOP-T6OG5FF>
2024-12-06 14:44:12 +0300

    update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            string task = "83 + 9 * 6 /31 - 10";
+            string task = "83 + 9 * 6 /31 - 10"; //res = 74,74..
             string tmp = "";
             List<int> objects = new List<int>();
             List<char> tasks = new List<char>();
@@ -40,7 +40,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
                     tasks.Add(task[i]);
                 }
             }
-            // Output
+            //DEBUG Output
             for (int i = 0; i < objects.Count; i++)
             {
                 Console.WriteLine(objects[i]);
@@ -48,6 +48,17 @@ namespace MyApp // Note: actual namespace depends on the project name.
             for (int i = 0; i < tasks.Count; i++)
             {
                 Console.WriteLine(tasks[i]);
+            }
+            // Core task
+            double res = 0;
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                if (tasks[i] == '*')
+                {
+                    res += objects[i] * objects[i + 1];
+                }
+                // Add other tassk
+                // Add priority
             }
         }
     }
