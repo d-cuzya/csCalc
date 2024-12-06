cb54204c7b7c0537b40eb8f63cad5957b11bbdbe
d-cuzya <Admin@DESKTOP-T6OG5FF>
2024-12-06 13:26:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string task = "8 + 9 * (6 /3)";
+            Console.WriteLine();
+        }
+    }
+}
